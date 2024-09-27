bb49a321a5f561736073985d763cde9ec3d68af7
thomaschunghow-univ-tlse3 <thomas.chung-how@univ-tlse3.fr>
2024-09-27 09:39:28 +0200

    à finir TD2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,27 +3,30 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements;
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
-		assert(force >= 0);
+		assert (force >= 0);
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 	}
 
 	public String getNom() {
 		return nom;
 	}
 
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + " »");
-	}
-
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
 
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + " »");
+	}
+
 	public void recevoirCoup(int forceCoup) {
-		assert(forceCoup >= 0);
+		assert (forceCoup >= 0);
 		int forceInitiale;
 		forceInitiale = force;
 		force -= forceCoup;
@@ -35,6 +38,19 @@ public class Romain {
 		assert (forceInitiale > force);
 	}
 
+	public void sEquiper(Equipement equipement) {
+		switch (equipement) {
+		case CASQUE: {
+			break;
+		}
+		case BOUCLIER: {
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + equipement);
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain romain;
 		romain = new Romain("Romain", 5);
@@ -44,5 +60,8 @@ public class Romain {
 		romain.recevoirCoup(2);
 		romain.recevoirCoup(2);
 		romain.recevoirCoup(2);
+		Equipement equipement = Equipement.BOUCLIER;
+		System.out.println(equipement.BOUCLIER);
+		System.out.println(equipement.CASQUE);
 	}
 }
